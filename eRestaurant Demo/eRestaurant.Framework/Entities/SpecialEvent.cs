8c0ee99a28bd6f1cc3033a1d73fd0fa69860c163
Eirik Skretteberg <eskretteberg1@studentmail.nait.ca>
2015-09-28 15:49:14 -0600

    Added more attributes, different ways to do them shown

--- eRestaurant Demo/eRestaurant.Framework/Entities/SpecialEvent.cs
+++ eRestaurant Demo/eRestaurant.Framework/Entities/SpecialEvent.cs
@@ -10,10 +10,22 @@ namespace eRestaurant.Framework.Entities
     public class SpecialEvent
     {
         [Key]
+        [Required(ErrorMessage = "An event code is required (one character only")]
+        [StringLength(1, ErrorMessage = "Event Codes only use a single character")]
         public string EventCode { get; set; }
+
+        [Required()]
+        [StringLength(30, MinimumLength = 5)]
         public string Description { get; set; }
         public bool Active { get; set; }
         // Navigation Properties
         public virtual ICollection<Reservations> Reservations { get; set; }
+
+        public SpecialEvent()
+        {
+            Active = true;
+            // To avoid null-reference errors for our navigation property
+            Reservations = new HashSet<Reservations>();
+        }
     }
 }
