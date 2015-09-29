5e76fcf6eaf36d4d18868e027196ea7bc3cda343
Eirik Skretteberg <eskretteberg1@studentmail.nait.ca>
2015-09-29 14:54:54 -0600

    Added some attributes

--- eRestaurant Demo/eRestaurant.Framework/Entities/Item.cs
+++ eRestaurant Demo/eRestaurant.Framework/Entities/Item.cs
@@ -11,10 +11,17 @@ namespace eRestaurant.Framework.Entities
     {
         [Key]
         public int ItemID { get; set; }
+
+        [Required, StringLength(35, MinimumLength = 5)]
         public string Description { get; set; }
+
+        [Range(0.01, 50.00)]
         public double CurrentPrice { get; set; }
+        [Range(0.01, 30.00)]
         public double CurrentCost { get; set; }
         public bool Active { get; set; }
+
+        []
         public int? Calories { get; set; }
         public string Comment { get; set; }
         public int MenuCategoryID { get; set; }
